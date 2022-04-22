1f3e9412c1bc1bd6aa7f32b069760bc7a0597ef7
DAIB OMOTAYO <daibomotayo@DAIBs-MBP.lan>
2022-04-21 21:50:08 -0500

    first terraform code

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAUCP3T4TSSR532BHI"
+  secret_key = "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"
+}
+
+
+resource "aws_instance" "class1" {
+  ami           = var.ami_id
+  # count         = var.instance_count 
+
+  for_each = {
+    prod = "t3.small"
+    test = "t2.nano"
+    preprod = "t3.medium"
+    sandbox = "t2.large"
+    qa = "t2.micro"
+    Development = "t3.medium"
+  }   
+  
+  instance_type = each.value
+  key_name      = "A4L"
+
+  tags = {
+    Name = each.key
+  }
+}
+
+
+variable "ami_id" {
+  default = "ami-03ededff12e34e59e"
+}
+
+# variable "instance_count" {
+#   default = 5
+# }
+
+# variable "instance_type" {
+#   default = each.value
+# }
+
+# variable "name" {
+#   default = each.key
+# }
