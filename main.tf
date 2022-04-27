3e2b81d7319673b5bd85be4db2d10079081d2560
DAIB OMOTAYO <daibomotayo@DAIBs-MBP.lan>
2022-04-26 21:36:06 -0500

    added two new instances

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "s3" {
-    bucket = "statebubket-1"
-    key    = "mystate1"
-    region = "us-east-1"
+    bucket  = "statebubket-1"
+    key     = "mystate1"
+    region  = "us-east-1"
     profile = "terraform-class"
   }
 }
@@ -21,12 +21,14 @@ resource "aws_instance" "class1" {
   ami = var.ami_id
 
   for_each = {
-    prod        = "t3.small"
-    test        = "t2.nano"
-    preprod     = "t3.medium"
-    sandbox     = "t2.large"
-    qa          = "t2.micro"
-    Development = "t2.micro"
+    prod           = "t3.small"
+    test           = "t2.nano"
+    preprod        = "t3.medium"
+    sandbox        = "t2.large"
+    qa             = "t2.micro"
+    Development    = "t2.micro"
+    toluinstance   = "t2.nano"
+    loladeinstance = "t3.medium"
   }
 
   instance_type = each.value
