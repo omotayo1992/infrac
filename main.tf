4feb143451fbff3afd11a66025beea958baaf8c1
DAIB OMOTAYO <daibomotayo@DAIBs-MBP.lan>
2022-05-11 21:32:30 -0500

    added rule 8080

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ terraform {
 
 
 resource "aws_instance" "jenkins" {
-  ami             = "ami-0022f774911c1d690"
-  instance_type   = "t2.micro"
-  key_name        = "A4L"
+  ami                    = "ami-0022f774911c1d690"
+  instance_type          = "t2.micro"
+  key_name               = "A4L"
   vpc_security_group_ids = [aws_security_group.allow_access.id]
 
   tags = {
@@ -49,12 +49,19 @@ resource "aws_security_group" "allow_access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "jenkins from public"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
